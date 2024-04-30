182b3655cd795efafa99e9084a6376963b0a3df9
Peter <pje4135@gmail.com>
2024-04-29 23:18:51 -0400

    Finished creation of adjaceny list: cutoff = .5

--- src/main.rs
+++ src/main.rs
@@ -17,12 +17,13 @@ fn read_data(path: &str) -> HashMap<u64, Vec<GameEntry>>{
     let file = File::open(path).expect("Couldn't Open");
     let mut buf_reader = std::io::BufReader::new(file).lines();
     buf_reader.next();
+    let mut game_id = 0;
     for line in buf_reader {
         let line_str = line.expect("Error Reading");
         let events: Vec<String> = line_str.split(",").map(|s| s.to_string()).collect();
         // println!("{:?},{:?}",events[1],events[0]);
         let game_entry = GameEntry {
-            game_id: u64::from_str(&events[0]).unwrap(),
+            game_id,
             minute: f64::from_str(&events[1]).unwrap(),
             description: events[5].clone(),
             home_club_goals: u64::from_str(&events[13]).unwrap(),
@@ -30,6 +31,7 @@ fn read_data(path: &str) -> HashMap<u64, Vec<GameEntry>>{
             total_goals: u64::from_str(&events[13]).unwrap() + u64::from_str(&events[14]).unwrap()
         };
         result.entry(game_entry.game_id).or_insert(Vec::new()).push(game_entry);
+        game_id += 1;
         if result.len() == 500 {
             break
         }
@@ -56,10 +58,10 @@ fn calculate_similarity(entry1:Vec<GameEntry>,entry2:Vec<GameEntry>) -> f64{
                weight_sum += weight;
             }
             if event.description==event2.description && event.description != "" {
-                weighted_sum -= 5.0;
+                weighted_sum -= 3.0;
             }
-            weighted_sum += 5.0;
-            weight_sum += 5.0;
+            weighted_sum += 3.0;
+            weight_sum += 3.0;
             sim_average += weighted_sum / weight_sum;
         }
     }
@@ -72,31 +74,35 @@ fn make_edge_list(entry_map:HashMap<u64,Vec<GameEntry>>) -> Vec<(f64, u64, u64)>
         for (game_id2, game_entries2) in &entry_map {
             if game_id1!= game_id2 {
                 let similarity_score = calculate_similarity(game_entries1.to_vec(), game_entries2.to_vec());
-                edges.push((similarity_score, *game_id1, *game_id2));
+                if similarity_score<0.5 {
+                    edges.push((similarity_score, *game_id1, *game_id2));
+                }
             }
         }
     }
     edges
 }
-fn create_similarity_matrix(vertices:usize,edges:Vec<(f64,u64,u64)>) {
-    let mut graph_matrix = vec![vec![false;vertices];vertices];
-    for (similarity, game1,game2) in edges.iter() {
-        graph_matrix[*game1 as usize][*game2 as usize] = true;
-        graph_matrix[*game2 as usize][*game1 as usize] = true; 
-    };
-    for row in &graph_matrix {
-        for entry in row.iter() {
-            print!(" {} ",if *entry {"1"} else {"0"});
+fn create_adjaceny_list(vertices:usize,edges:Vec<(f64,u64,u64)>) {
+    let mut graph_list : Vec<Vec<usize>> = vec![vec![];vertices];
+    for (_,v,w) in edges.iter() {
+        if!graph_list[*v as usize].contains(&(*w as usize)) {
+            graph_list[*v as usize].push(*w as usize);
         }
-        println!("");
+        if!graph_list[*w as usize].contains(&(*v as usize)) {
+            graph_list[*w as usize].push(*v as usize);
+        }
+    };
+    for i in 0..graph_list.len() {
+        println!("{}: {:?}", i, graph_list[i]);
     };
 }
 fn main() {
    let entries=read_data("C:/Users/pje41/Final_Project_210/soccer-data/processed_data/game_data.csv");
    let vertices = entries.len();
    let edges: Vec<(f64,u64,u64)> = make_edge_list(entries);
-   create_similarity_matrix(vertices, edges)
-    // Check one game_id against all other game_ids 
+   println!("{:?}",edges);
+   create_adjaceny_list(vertices, edges)
+    // Check one game_id againast all other game_ids 
     //with each event being compared to each one and
     // an average score of the comparison being the simialrity for that event
 }
